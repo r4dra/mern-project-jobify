4d5f5acdb83a41166f7bcdbdaf3f30739b711561
Rudra Pratap Singh Tomar <RudraPratapSingh.Tomar@in.pega.com>
2022-11-11 03:32:56 +0530

    Section 7 completed

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/Testing';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>I am daddy</p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
